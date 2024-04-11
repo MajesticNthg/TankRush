a14e5db5be098d6d01bbef8732b13b0add103681
MajesticNthg <majestic_nthg@mail.ru>
2024-04-11 20:12:33 +1000

    mod.1

--- src/Level1.java
+++ src/Level1.java
@@ -1,17 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Level1 {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
+        if (H1 < H2 || W1 < W2) return false;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        String[] words = S2.split(" ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for (String word : words) {
+            if (S1.contains(word)) continue;
+            else return false;
         }
+        return true;
     }
 }
+
